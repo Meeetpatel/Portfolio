c4db0ca5ee86935a7bf7259d3a5372d27ac39ef2
Meet Patel <meetpatel4567.mp@gmail.com>
2023-12-18 03:51:14 -0500

    final commit

--- src/Components/NewsGoose.js
+++ src/Components/NewsGoose.js
@@ -4,6 +4,7 @@ import "aos/dist/aos.css"; // Import the AOS styles
 import newsgoose from "./NewsGoose.mp4";
 import { Link } from "react-router-dom";
 import "./contact.css";
+import "./newsgoose.css";
 export default function NewsGoose() {
   useEffect(() => {
     Aos.init({ duration: 2000 });
@@ -18,29 +19,11 @@ export default function NewsGoose() {
         Work
       </Link>
       <div className="d-flex text-align-center justify-content-center mt-5">
-        <h1
-          style={{
-            fontFamily: "Arial, Helvetica, sans-serif",
-            fontWeight: "600",
-            color: "orangered",
-            fontSize: "4vw",
-          }}
-          data-aos="fade-down"
-        >
+        <h1 data-aos="fade-down" className="newshead">
           NewsGoose
         </h1>
       </div>
-      <div
-        style={{
-          textAlign: "center",
-          fontSize: "2vw",
-          fontFamily: "Arial, Helvetica, sans-serif",
-          marginLeft: "10%",
-          marginRight: "10%",
-          marginTop: "10%",
-          margin: "10%",
-        }}
-      >
+      <div className="newspara">
         <p data-aos="flip-left">
           I developed this App using React.js, integrating a news API to fetch
           real-time data and displayed it using dynamic CSS with the help of
@@ -54,7 +37,6 @@ export default function NewsGoose() {
       <div className="d-flex text-align-center justify-content-center mt-5">
         <video data-aos="fade-down" width="90%" height="50%" controls>
           <source src={newsgoose} type="video/mp4" />
-          Your browser does not support the video tag.
         </video>
       </div>
     </div>
