60236b456815288fb83efc6b91d36336e94a2552
Meet Patel <meetpatel4567.mp@gmail.com>
2024-01-18 01:43:59 -0500

    Profiles added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./home.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import linkedin from "./linkedin.png";
+import github from "./github.png";
 
 export default function Home() {
-  const Home = () => {
-    useEffect(() => {
-      Aos.init({ duration: 2000 });
-    }, []);
-  };
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   const [hovered, setHovered] = useState(false);
   const [Workhovered, setWorkHovered] = useState(false);
   const [Contacthovered, setContactHovered] = useState(false);
@@ -22,6 +22,7 @@ export default function Home() {
   const handleMouseOut = () => {
     setHovered(false);
   };
+
   const handleMouseOverWork = () => {
     setWorkHovered(true);
   };
@@ -29,6 +30,7 @@ export default function Home() {
   const handleMouseOutWork = () => {
     setWorkHovered(false);
   };
+
   const handleMouseOverContact = () => {
     setContactHovered(true);
   };
@@ -38,39 +40,58 @@ export default function Home() {
   };
 
   return (
-    <div className="home-container">
-      <div className="hello">
+    <>
+      <div className="social">
         <Link
-          to="/About"
-          className={`hello ${hovered ? "hovered" : ""}`}
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
+          className="social2"
+          onClick={() =>
+            window.open("https://www.linkedin.com/in/meet-patel25/", "_blank")
+          }
         >
-          {hovered ? "About" : "Hello."}
+          <img src={linkedin} width="50" alt="Linkedin" />
+        </Link>
+
+        <Link
+          className="social2 mt-5"
+          onClick={() => window.open("https://github.com/Meeetpatel", "_blank")}
+        >
+          <img src={github} width="33" alt="github" />
         </Link>
       </div>
-      <div className="meet">
-        <Link
-          to="/Work"
-          className={`meet ${Workhovered ? "hovered" : ""}`}
-          onMouseOver={handleMouseOverWork}
-          onMouseOut={handleMouseOutWork}
-        >
-          {" "}
-          {Workhovered ? "My Work" : "I am"}
-        </Link>
+      <div className="home-container">
+        <div className="hello">
+          <Link
+            to="/About"
+            className={`hello ${hovered ? "hovered" : ""}`}
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+          >
+            {hovered ? "About" : "Hello."}
+          </Link>
+        </div>
+        <div className="meet">
+          <Link
+            to="/Work"
+            className={`meet ${Workhovered ? "hovered" : ""}`}
+            onMouseOver={handleMouseOverWork}
+            onMouseOut={handleMouseOutWork}
+          >
+            {" "}
+            {Workhovered ? "My Work" : "I am"}
+          </Link>
+        </div>
+        <div className="meet2">
+          <Link
+            to="/Contact"
+            className={`meet2 ${Contacthovered ? "hovered" : ""}`}
+            onMouseOver={handleMouseOverContact}
+            onMouseOut={handleMouseOutContact}
+          >
+            {" "}
+            {Contacthovered ? "Contact" : "Meet"}
+          </Link>
+        </div>
       </div>
-      <div className="meet2">
-        <Link
-          to="/Contact"
-          className={`meet2 ${Contacthovered ? "hovered" : ""}`}
-          onMouseOver={handleMouseOverContact}
-          onMouseOut={handleMouseOutContact}
-        >
-          {" "}
-          {Contacthovered ? "Contact" : "Meet"}
-        </Link>
-      </div>
-    </div>
+    </>
   );
 }
